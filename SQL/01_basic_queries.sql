c8e7ae072a28ec17e031defdd357c56e30d25226
Shiwangi Rai <raishiwangi17@gmail.com>
2026-09-24 19:47:38 +0530

    Enhance SQL file with additional queries and explanations
    Added various SQL queries demonstrating the use of NULL, AND, OR, and NOT conditions, along with practice questions for filtering campaigns based on different criteria.

--- SQL/01_basic_queries.sql
+++ SQL/01_basic_queries.sql
@@ -53,7 +53,112 @@ Select * from `performancedatashiwangi.main.pm` where device= 'desktop' order by
 ---List top 5 most clicked campaigns for brand subchannel
 Select campaign_platform,subchannel, clicks from `performancedatashiwangi.main.pm` where subchannel= 'brand' order by clicks desc limit 5;
 
+---NUll Function
+---null represents missing or unknown data. It means the value is not present in the database 
+--- we cannot use = or ! to compare with NULL instead use:
+---Is Null to check if value is missing
+---Is NOT NULL to check if value is existing
 
+---GET ALL RECORDS where audience_type is missing
+Select * from `performancedatashiwangi.main.pm` where audience_type is null;
+
+---list rows where reative_type is not missing
+Select * from `performancedatashiwangi.main.pm` where creative_type is not null;
+
+
+---AND---------------------------------------------------------------------
+---- It allows you to apply multiple condition in the where claAUSE A ROQ MUST MEET ALL CONDITIONS TO BE RETURNED
+
+--- Get data where device is mobile and age is 25-34
+Select * from `performancedatashiwangi.main.pm` where device= 'Mobile' and age= '24-35';
+
+---show campaigns on facebook ads with impressions more than 500
+Select * from `performancedatashiwangi.main.pm` where campaign_type ='Facebook Ads' and impressions > 500;
+
+
+---- OR--------------------------------------------------------------------
+----It returns data when atleast one contion is true, used when we want flexibility in filtering
+
+----show rows where device is mobile or connected TV
+Select * from `performancedatashiwangi.main.pm` where device= 'Mobile' Or device='Connect TV';
+
+
+---NOT-------------------------------
+---Use to negate a condition. It return rows where condition is not true
+------ use with where (WHERE NOT)
+--- Show where device is not mobile
+Select * from `performancedatashiwangi.main.pm` where not device = 'Mobile';
+
+
+
+
+-----Practice questions------
+
+-----Retrieve non desktop campaigns with impressions less than 1000
+
+Select * from `performancedatashiwangi.main.pm` where device <> 'Desktop' and impressions<1000;
+--- [<>] we can use thiws symbol at place of not
+
+---Retrieve facebook ads campaigns where clicks are between 50 and 200
+Select * from `performancedatashiwangi.main.pm` where campaign_type= 'Facebook ads' and clicks > 50 and clicks < 200;
+
+---Show audience type is missing and spends >100
+Select * from `performancedatashiwangi.main.pm` where audience_type is NULL and spends >100;
+
+---- Retrieve campaigns where age is 35-44 or 45-54 and spends <100
+Select * from `performancedatashiwangi.main.pm` where (age = '35-44' or age = '45-54') and spends <100;
+
+----Exclude camopaigns with device tablet and age 18-24
+Select * from `performancedatashiwangi.main.pm` where not (device ='tablet' and age = '18-24');
+
+---Top 5 mobile campaigns by link clicks
+Select * from `performancedatashiwangi.main.pm` where device = 'Mobile'order by link_clicks DESC Limit 5;
+
+---List Campaigns that are not in search and not in the age group 25-34
+Select * from `performancedatashiwangi.main.pm` where not (campaign_type = 'Search' and age = '25-34');
+
+
+---Get campaigns for users aged 65 or more with clicks above 10
+Select * from `performancedatashiwangi.main.pm` where age = '65 or more' and clicks > 10;
+
+---Retrieve all Facebook Campaigns where subchannel is brand or generic
+Select * from `performancedatashiwangi.main.pm` where campaign_type = 'facebook ads' and subchannel ='Brand or Generic';
+
+---Explanantion: Focus on content targeting brand/genric terms on facebook
+
+
+---Find campaigns with NULL creative type and not null audience type
+Select * from `performancedatashiwangi.main.pm` where creative_type is null and audience_type is not null;
+
+---Show non google ads campaigns targeting age 35-44 or 45-54
+Select * from `performancedatashiwangi.main.pm` WHERE NOT campaign_platform = 'Google Ads' and (age = '35-44'OR age = '45-54');
+
+
+---Find top 10 campaigns by impressions for connected TV
+Select * from `performancedatashiwangi.main.pm` where device = 'Connected TV' order by impressions desc limit 10;
+
+---  Retrieve Records with age not in '18-24' or '25-34'
+Select * from `performancedatashiwangi.main.pm` where not (age ='18-24' or age = '25-34');
+
+
+---Retrieve top 5 records with highest clicks for google ads
+Select * from `performancedatashiwangi.main.pm` where campaign_platform = 'Google Ads'order by clicks desc limit 5;
+
+----Show campaigns where device is not mobile and clicks are less than 20
+Select * from `performancedatashiwangi.main.pm` where not device = 'Mobile' and clicks < 20;
+
+
+---Retrieve campaigns with spends> 100 and device either or  tablet
+Select * from `performancedatashiwangi.main.pm` where spends > 100 and (device = 'Desktop' or device = 'Tablet');
+
+---List all campaigns where link_click is null and impressions is grwater than 500
+Select * from `performancedatashiwangi.main.pm` where link_clicks is null and impressions > 500;
+
+---Retrieve campaigns not targeted to undertmined age
+Select * from `performancedatashiwangi.main.pm` where not  age = 'undetermind';
+
+---List campaign with either low clicks (<5) or low spends (<10)
+Select * from `performancedatashiwangi.main.pm` where clicks < 5 or spends < 10;
 
 
 
